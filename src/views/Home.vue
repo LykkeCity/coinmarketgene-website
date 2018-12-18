20f4947ffa244e60b3a4fb2cbce1d282209ac638
amigin <33rkjgerhjg51>
2018-12-19 00:32:49 +0200

    made table of coins requestable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,27 +6,32 @@
           <thead>
             <tr>
               <th scope="col">
-                <div class="sort_control">Exchange
+                <div class="sort_control">
+                  Exchange
                   <i class="icon icon--arrow"></i>
                 </div>
               </th>
               <th scope="col">
-                <div class="sort_control">Volume
+                <div class="sort_control">
+                  Volume
                   <i class="icon icon--arrow"></i>
                 </div>
               </th>
               <th scope="col">
-                <div class="sort_control">Effective price (asset)
+                <div class="sort_control">
+                  Effective price (asset)
                   <i class="icon icon--arrow"></i>
                 </div>
               </th>
               <th scope="col">
-                <div class="sort_control">Depth
+                <div class="sort_control">
+                  Depth
                   <i class="icon icon--arrow"></i>
                 </div>
               </th>
               <th scope="col" class="text-right">
-                <div class="sort_control">Total cost
+                <div class="sort_control">
+                  Total cost
                   <i class="icon icon--arrow"></i>
                 </div>
               </th>
@@ -34,276 +39,38 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
+            <tr v-for="item in items">
               <td>
                 <a href>
                   <div class="exchange_item">
                     <div class="exchange_item__logo">
-                      <img
-                        src="images/exchange/binance-coin-logo-png-transparent.png"
-                        alt="Binance"
-                        width="24px"
-                      >
+                      <img :src="item.img" alt="Binance" width="24px">
                     </div>
-                    <span class="exchange_item__name">Binance</span>
+                    <span class="exchange_item__name">{{item.exchange}}</span>
                   </div>
                 </a>
               </td>
               <td>
                 <a href="info.html">
-                  <span class="_value">$ 833,746,525</span>
+                  <span class="_value">{{item.volume}}</span>
                 </a>
               </td>
               <td>
                 <a href="info.html">
-                  <span class="_value">$ 833,746,525</span>
+                  <span class="_value">{{item.effectiveprice}}</span>
                 </a>
               </td>
               <td>
-                <span class="_value">BTC 833,746,525</span>
+                <span class="_value">{{item.depth}}</span>
               </td>
               <td class="text-right">
-                <span class="badge badge-success">9.91%</span>
-              </td>
-              <td class="text-right">
-                <div class="chart_preview">
-                  <img
-                    src="images/chart.png"
-                    alt="chart"
-                    class="chart_preview__dummy"
-                    width="143px"
-                  >
-                </div>
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                <div class="exchange_item">
-                  <div class="exchange_item__logo">
-                    <img src="images/exchange/okex.png" alt="OKEx" width="24px">
-                  </div>
-                  <span class="exchange_item__name">OKEx</span>
-                </div>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">BTC 833,746,525</span>
-              </td>
-              <td class="text-right">
-                <span class="badge badge-danger">9.91%</span>
+                <span
+                  v-bind:class="{ 'badge badge-success':item.totalcost>0, 'badge badge-danger':item.totalcost<0 }"
+                >{{item.totalcost}}%</span>
               </td>
               <td class="text-right">
                 <div class="chart_preview">
-                  <img
-                    src="images/chart.png"
-                    alt="chart"
-                    class="chart_preview__dummy"
-                    width="143px"
-                  >
-                </div>
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                <div class="exchange_item">
-                  <div class="exchange_item__logo">
-                    <img src="images/exchange/huobi.png" alt="Huobi" width="15px">
-                  </div>
-                  <span class="exchange_item__name">Huobi</span>
-                </div>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">BTC 833,746,525</span>
-              </td>
-              <td class="text-right">
-                <span class="badge badge-danger">9.91%</span>
-              </td>
-              <td class="text-right">
-                <div class="chart_preview">
-                  <img
-                    src="images/chart.png"
-                    alt="chart"
-                    class="chart_preview__dummy"
-                    width="143px"
-                  >
-                </div>
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                <div class="exchange_item">
-                  <div class="exchange_item__logo">
-                    <img src="images/exchange/digifinex.png" alt="DigiFinex" width="24px">
-                  </div>
-                  <span class="exchange_item__name">DigiFinex</span>
-                </div>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">BTC 833,746,525</span>
-              </td>
-              <td class="text-right">
-                <span class="badge badge-danger">9.91%</span>
-              </td>
-              <td class="text-right">
-                <div class="chart_preview">
-                  <img
-                    src="images/chart.png"
-                    alt="chart"
-                    class="chart_preview__dummy"
-                    width="143px"
-                  >
-                </div>
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                <div class="exchange_item">
-                  <div class="exchange_item__logo">
-                    <img src="images/exchange/upbit.png" alt="Upbit" width="24px">
-                  </div>
-                  <span class="exchange_item__name">Upbit</span>
-                </div>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">BTC 833,746,525</span>
-              </td>
-              <td class="text-right">
-                <span class="badge badge-success">9.91%</span>
-              </td>
-              <td class="text-right">
-                <div class="chart_preview">
-                  <img
-                    src="images/chart.png"
-                    alt="chart"
-                    class="chart_preview__dummy"
-                    width="143px"
-                  >
-                </div>
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                <div class="exchange_item">
-                  <div class="exchange_item__logo">
-                    <img src="images/exchange/coinbene.png" alt="CoinBene" width="24px">
-                  </div>
-                  <span class="exchange_item__name">CoinBene</span>
-                </div>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">BTC 833,746,525</span>
-              </td>
-              <td class="text-right">
-                <span class="badge badge-success">9.91%</span>
-              </td>
-              <td class="text-right">
-                <div class="chart_preview">
-                  <img
-                    src="images/chart.png"
-                    alt="chart"
-                    class="chart_preview__dummy"
-                    width="143px"
-                  >
-                </div>
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                <div class="exchange_item">
-                  <div class="exchange_item__logo">
-                    <img src="images/exchange/zb.png" alt="ZB.com" width="24px">
-                  </div>
-                  <span class="exchange_item__name">ZB.com</span>
-                </div>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">BTC 833,746,525</span>
-              </td>
-              <td class="text-right">
-                <span class="badge badge-danger">9.91%</span>
-              </td>
-              <td class="text-right">
-                <div class="chart_preview">
-                  <img
-                    src="images/chart.png"
-                    alt="chart"
-                    class="chart_preview__dummy"
-                    width="143px"
-                  >
-                </div>
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                <div class="exchange_item">
-                  <div class="exchange_item__logo">
-                    <img src="images/exchange/dobi.png" alt="DOBI" width="24px">
-                  </div>
-                  <span class="exchange_item__name">DOBI</span>
-                </div>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">$ 833,746,525</span>
-              </td>
-              <td>
-                <span class="_value">BTC 833,746,525</span>
-              </td>
-              <td class="text-right">
-                <span class="badge badge-success">9.91%</span>
-              </td>
-              <td class="text-right">
-                <div class="chart_preview">
-                  <img
-                    src="images/chart.png"
-                    alt="chart"
-                    class="chart_preview__dummy"
-                    width="143px"
-                  >
+                  <img :src="item.chartimg" alt="chart" class="chart_preview__dummy" width="143px">
                 </div>
               </td>
             </tr>
@@ -317,9 +84,42 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+//import Vue from "vue";
+//Vue.use(vueResource);
 
 export default {
   name: "home",
+  data() {
+    return {
+      items: [
+        {
+          img: "/images/exchange/binance-coin-logo-png-transparent.png",
+          exchange: "Binance",
+          volume: "$ 833,746,525",
+          effectiveprice: "$ 833,746,525",
+          depth: "BTC 833,746,525",
+          totalcost: "9.91",
+          chartimg: "/images/chart.png"
+        }
+      ]
+    };
+  },
+
+  created: function() {
+    this.getItems();
+  },
+
+  methods: {
+    getItems: function() {
+      console.log(this);
+      this.$http
+        .get("https://public-api.lykke.com/api/IsAlive")
+        .then(function(response) {
+          console.log(response);
+          //  this.message = response;
+        });
+    }
+  },
   components: {
     HelloWorld
   }
